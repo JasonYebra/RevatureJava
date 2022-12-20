6634185258d7cab2eb96158b915457bba96011b6
JasonYebra <jasonyebra@gmail.com>
2022-12-20 22:52:57 +0000

    Live lecture

---
+++ LiveLectures/W1/d2/Calculator.java
@@ -0,0 +1,19 @@
+package LiveLectures.W1.d2;
+import java.util.Scanner;
+
+public class Calculator {
+    /*
+    *I want to make a calculator app
+    *for the start, I want to be able to to multiple and divide numbers
+    *@param args
+    */ 
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input first number:");
+        int n1 = sc.nextInt();
+        System.out.println("input second number:");
+        int n2 = sc.nextInt();
+        System.out.println("input 'multiply' or 'divide':");
+    }   
+}
---
+++ revpro/HelloWorld.java
@@ -0,0 +1,5 @@
+package revpro;
+
+public class HelloWorld {
+    
+}
